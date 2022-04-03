a6b029327b8581b98f14589e8f780ee6abd23974
Ricardo Jenez <76978529+rjenez@users.noreply.github.com>
2022-04-02 22:25:02 -0700

    removed reference to gdrive.

--- scripts/generatedata.sh
+++ scripts/generatedata.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 #python processhomework.py --source='../data/programming-homework-dataset-plagiarism-detection/src' --desc='../data/programming-homework-dataset-plagiarism-detection/ground-truth-static-anon.txt' --odir='../data/programming-homework-dataset-plagiarism-detection' --samples=10
 python processhomework.py --source='../data/programming-homework-dataset-plagiarism-detection/src' --desc='../data/programming-homework-dataset-plagiarism-detection/ground-truth-static-anon.txt' --odir='.' --samples=$1
-tar cvf plag.tar train.csv test.csv
-gdrive upload plag.tar
+
 
